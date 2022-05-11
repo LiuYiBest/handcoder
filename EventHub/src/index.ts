16422adf67bfd667a6418952098c644d0ce43d60
liuyidiao <1070692044@qq.com>
2022-05-11 22:48:20 +0800

    发布订阅模式

--- EventHub/src/index.ts
+++ EventHub/src/index.ts
@@ -1,4 +1,8 @@
-//新建EventHub类并导出
-class EventHub {}
+//新建EventHub类并导出   发布订阅模式
+class EventHub {
+    on(eventName,fn){}       //事件名  处理函数
+    emit(eventName){}
+}
 
 export  default EventHub
+
--- EventHub/test/index.ts
+++ EventHub/test/index.ts
@@ -1,2 +1,12 @@
 import EventHub from "../src";
+
 console.log(EventHub)
+
+const eventHub = new EventHub()
+console.log('是否是对象',eventHub instanceof Object)
+
+//on emit
+
+eventHub.on("xxx",()=>{
+    console.log('被调用')
+})
