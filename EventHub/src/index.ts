72428a029567e6efc1982f0cb7a5eb28afec3e4c
liuyidiao <1070692044@qq.com>
2022-05-11 22:50:53 +0800

    发布订阅模式

--- EventHub/src/index.ts
+++ EventHub/src/index.ts
@@ -1,7 +1,7 @@
 //新建EventHub类并导出   发布订阅模式
 class EventHub {
-    on(eventName,fn){}       //事件名  处理函数
-    emit(eventName){}
+    on(eventName,fn){}       //发布参数1.事件名  2.处理函数
+    emit(eventName){}        //订阅
 }
 
 export  default EventHub
